6212600d6be716b5098ccc8ffd6bf966b90b3444
lejhy <flejhanec@seznam.cz>
2017-02-17 20:56:23 +0000

    Passing vertex color from vertex shader to fragment shader
    At least it doesn't only have one color...

--- Main.cpp
+++ Main.cpp
@@ -10,18 +10,21 @@
 const GLuint WIDTH = 1920, HEIGHT = 1080;
 
 // Shaders
-const GLchar *vertexShaderSource = 
+const GLchar *vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 position;\n"
+"out vec4 vertexColor;\n"
 "void main () {\n"
 "	gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"	vertexColor = vec4(position.x, position.y, position.z, 1.0);\n"
 "}\0";
 
 const GLchar *fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 color;\n"
+"in vec4 vertexColor;\n"
 "void main () {\n"
-"	color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"	color = vertexColor;\n"
 "}\0";
 
 //functions
