ad459de73bd3c4a0fa82d0a697ac3abc17d98cb6
lejhy <flejhanec@seznam.cz>
2017-02-18 23:37:11 +0000

    Camera and movement

--- Main.cpp
+++ Main.cpp
@@ -17,11 +17,30 @@
 // Other includes
 #include "shader.h"
 
+//functions
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void do_movement();
+
 // Window dimensions
 const GLuint WIDTH = 1920, HEIGHT = 1080;
 
-//functions
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+// Create a camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+const GLfloat movementSpeed = 5.0f;
+const GLfloat mouseSensitivity = 0.05f;
+
+// Controls
+bool keys[1024];
+GLfloat mouseX = 0, mouseY = 0;
+GLfloat yaw = -90.0f, pitch = 0.0f;
+bool firstMouseMovement = true;
+
+// Timing
+GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
+GLfloat lastFrame = 0.0f;  	// Time of last frame
 
 // The MAIN function, from here we start the application and run the game loop
 int main() {
@@ -61,12 +80,14 @@ int main() {
 	glViewport(0, 0, viewportWidth, viewportHeight);
 	GLfloat aspectRatio = (GLfloat)viewportWidth / (GLfloat)viewportHeight;
 
-	// Assign the key callback to our window
+	// Assign the controls to our window
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// Make some geometry to work with
 	// Square
-	GLfloat squareVerts[] = {
+	GLfloat cubeVerts[] = {
 		-0.5f, -0.5f, -0.5f,	1.0f, 0.0f, 0.0f,	0.0f, 0.0f,
 		0.5f, -0.5f, -0.5f,  	0.0f, 1.0f, 0.0f,	1.0f, 0.0f,
 		0.5f,  0.5f, -0.5f,  	0.0f, 0.0f, 1.0f,	1.0f, 1.0f,
@@ -110,24 +131,24 @@ int main() {
 		-0.5f,  0.5f, -0.5f,  	1.0f, 1.0f, 0.0f,	0.0f, 1.0f
 	};
 	// Initialize variables
-	GLuint squareVAO, squareVBO, squareEBO;
-	glGenVertexArrays(1, &squareVAO);
-	glGenBuffers(1, &squareVBO);
-	glGenBuffers(1, &squareEBO);
+	GLuint cubeVAO, cubeVBO, cubeEBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glGenBuffers(1, &cubeEBO);
 	// Bind the Vertex Array Object
-	glBindVertexArray(squareVAO);
-		// Copy vertices array in a buffer for OpenGL to use
-		glBindBuffer(GL_ARRAY_BUFFER, squareVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(squareVerts), squareVerts, GL_STATIC_DRAW);
-		// Set vertex attributes pointers
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
-		glEnableVertexAttribArray(0);
-		// Color attribute
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-		glEnableVertexAttribArray(1);
-		// Set texture attributes pointers
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-		glEnableVertexAttribArray(2);
+	glBindVertexArray(cubeVAO);
+	// Copy vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
+	// Set vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	// Set texture attributes pointers
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 	// Unbind the Vertex Array Object
 	glBindVertexArray(0);
 
@@ -169,10 +190,35 @@ int main() {
 	stbi_image_free(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
+	// Transformations
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
+	// Get vertex shader uniform locations
+	GLint modelLoc = glGetUniformLocation(shader.Program, "model");
+	GLint viewLoc = glGetUniformLocation(shader.Program, "view");
+	GLint projectionLoc = glGetUniformLocation(shader.Program, "projection");
+
 	// Program loop
 	while (!glfwWindowShouldClose(window)) {
+		// Update time
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
+		do_movement();
 
 		// Do all the rendering
 
@@ -187,20 +233,14 @@ int main() {
 		shader.use();
 
 		// Transformations
-		// Model
-		glm::mat4 model;
-		model = glm::rotate(model, PI/4*time, glm::vec3(0.5f, 1.0f, 0.0f));
-		GLint modelLoc = glGetUniformLocation(shader.Program, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		// View
 		glm::mat4 view;
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		GLint viewLoc = glGetUniformLocation(shader.Program, "view");
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		// Projection
 		glm::mat4 projection;
 		projection = glm::perspective(PI/4, aspectRatio, 0.1f, 100.0f);
-		GLint projectionLoc = glGetUniformLocation(shader.Program, "projection");
+		
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		// Textures
@@ -212,9 +252,17 @@ int main() {
 		glUniform1i(glGetUniformLocation(shader.Program, "texture1"), 1);
 
 		// Square
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glBindVertexArray(squareVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(cubeVAO);
+		for (GLuint i = 0; i < 10; i++) {
+			glm::mat4 model;
+			model = glm::translate(model, cubePositions[i]);
+			GLfloat angleOffset = PI / 9 * i;
+			GLfloat angle = angleOffset + (PI / 4 * time);
+			model = glm::rotate(model, angle, glm::vec3(0.5f, 1.0f, 0.0f));
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
@@ -231,4 +279,55 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
+	// All other key inputs
+	if (action == GLFW_PRESS) {
+		keys[key] = true;
+	}
+	else if (action == GLFW_RELEASE) {
+		keys[key] = false;
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouseMovement) {
+		mouseX = xpos;
+		mouseY = ypos;
+		firstMouseMovement = false;
+	}
+
+	GLfloat xOffset = xpos - mouseX;
+	GLfloat yOffset = mouseY - ypos; // Reversed since y-coordinates range from bottom to top
+	mouseX = xpos;
+	mouseY = ypos;
+
+	xOffset *= mouseSensitivity;
+	yOffset *= mouseSensitivity;
+	yaw += xOffset;
+	pitch += yOffset;
+
+	if (pitch > 89.0f) {
+		pitch = 89.0f;
+	}
+	if (pitch < -89.0f) {
+		pitch = -89.0f;
+	}
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+	front.y = sin(glm::radians(pitch));
+	front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+	cameraFront = glm::normalize(front);
+}
+
+void do_movement() {
+	GLfloat cameraSpeed = movementSpeed * deltaTime;
+	// WASD Movement
+	if (keys[GLFW_KEY_W])
+		cameraPos += cameraSpeed * cameraFront;
+	if (keys[GLFW_KEY_S])
+		cameraPos -= cameraSpeed * cameraFront;
+	if (keys[GLFW_KEY_A])
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (keys[GLFW_KEY_D])
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
